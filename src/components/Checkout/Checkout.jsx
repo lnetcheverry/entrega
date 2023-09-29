c92b4f93cced2b7d28b584ac0d813e864146ae0e
lnetcheverry} <leo.etcheverry16@gmail.com>
2023-09-29 13:23:22 -0300

    ultimos pasos

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,64 +1,79 @@
+import { useState } from "react"
 import { useCart } from "../Context/CartContext"
 import { collection, query, where, documentId, getDocs, writeBatch, addDoc } from "firebase/firestore"
 import { db } from "../../service/firebaseConfing"
+import { useNavigate } from "react-router-dom"
+import CheckoutForm from "../CheckoutForm/CheckoutForm"
 
 const Checkout = () => {
-     const { cart, total } = useCart()
+     const [loading, setLoading] = useState(false)
+     const { cart, total, clearCart } = useCart()
 
-     const createOrder = async() => {
-     const objOrder = {
-          buyer: {    
-          name: 'Leonardo Etcheverry',
-          phone: '15531688',
-          email: 'leo.etcheverry16@gmail.com'
-        },
-       items: cart,
-       total
-   }   
+     const navigate = useNavigate()
 
-    const batch = writeBatch(db)
-    const outOfStock = []
+     const createOrder = async({Nombre, Telefono, Email}) => {
+        try {
+            setLoading(true)
+            const objOrder = {
+                buyer: {    
+                Nombre, Telefono, Email},
+             items: cart,
+             total
+         }   
+      
+          const batch = writeBatch(db)
+          const outOfStock = []
+      
+         const ids = cart.map(prod => prod.id)
+         console.log(ids)
+      
+         const productsRef = query(collection( db, 'products'), where(documentId(), 'in', ids))
+          
+          const { docs } = await getDocs(productsRef)
+      
+               docs.forEach(doc => {
+                  const fields = doc.data()
+                  const stockDb = fields.stock
+      
+                  const productAddedToCart = cart.find(prod => prod.id == doc.id)
+                  const prodQuantity = productAddedToCart?.quantity 
+      
+                  if(stockDb >= prodQuantity) {
+                      batch.update(doc.ref, { stock: stockDb - prodQuantity})
+                  } else {
+                      outOfStock.push({ id: doc.id, ...fields})
+                  }
+              })
+           
+              if(outOfStock.length==0) {
+                   const orderRef = collection(db, 'orders')
+      
+                  const { id: orderId} = await addDoc(orderRef,objOrder)
+      
+                   batch.commit()
+                   clearCart()
+                   navigate('/')
+                   console.log('el numero de orden  es: ' + orderId)
+      
+              } else {
+              console.error('fuera de stock ')
+              }
+        }catch (error){
+            console.log('Ocurrio un error al obtener datos: ' + error.message)
 
-   const ids = cart.map(prod => prod.id)
-   console.log(ids)
-
-   const productsRef = query(collection( db, 'products'), where(documentId(), 'in', ids))
-    
-    const { docs } = await getDocs(productsRef)
-
-         docs.forEach(doc => {
-            const fields = doc.data()
-            const stockDb = fields.stock
-
-            const productAddedToCart = cart.find(prod => prod.id == doc.id)
-            const prodQuantity = productAddedToCart?.quantity 
-
-            if(stockDb >= prodQuantity) {
-                batch.up(doc.ref, { stock: stockDb - prodQuantity})
-            } else {
-                outOfStock.push({ id: doc.id, ...fields})
-            }
-        })
-     
-        if(outOfStock.length==0) {
-             const orderRef = collection(db, 'orders')
-
-            const { id: orderId} = await addDoc(orderRef,objOrder)
-
-             batch.commit()
-
-             console.log('el numero de orden  es: ' + orderId)
-
-        } else {
-        console.error('fuera de stock ')
+        }finally {
+            setLoading(false)
         }
+
     }
      
+    if(loading) {
+        return <h1>Se esta generando su orden</h1>
+    }
     return (
         <>
-            <h1>Checkout</h1>
-            <h2>Formulario</h2>
-            <button onClick={createOrder}>Generar orden</button>        
+            <h1>Llene los datos para terminar la compra</h1>
+             <CheckoutForm onconfirm={createOrder} />     
         </>
     )
 }
--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 
+
 const CheckoutForm = ({ createOrden, onconfirm }) => {
   const [Nombre, setName] = useState('')
   const [Telefono, setPhone] = useState('')
@@ -17,15 +18,15 @@ const CheckoutForm = ({ createOrden, onconfirm }) => {
 
   return (
     <div>
-      <div className={Formulario.dividiendo}>
-        <form className={Formulario.form} onSubmit={handleConfirm}>
+      <div>
+        <form onSubmit={handleConfirm}>
           <label>Nombre</label>
-          <input className={Formulario.input} type="text" value={Nombre} onChange={({ target }) => setName(target.value)} placeholder="Escribe tu nombre Aqui" />
+          <input type="text" value={Nombre} onChange={({ target }) => setName(target.value)} placeholder="Escribe tu nombre Aqui" />
           <label>Telefono</label>
-          <input className={Formulario.input} type="number" value={Telefono} onChange={({ target }) => setPhone(target.value)} min={0} placeholder="Escribe tu numero de Telefono" />
+          <input  type="number" value={Telefono} onChange={({ target }) => setPhone(target.value)} min={0} placeholder="Escribe tu numero de Telefono" />
           <label>E-mail</label>
-          <input className={Formulario.input} type="email" value={Email} onChange={({ target }) => setEmail(target.value)} required placeholder="Escribe tu E-mail Aqui" />
-          <button className={Formulario.Generar} onClick={createOrden}>Generar Orden</button>
+          <input  type="email" value={Email} onChange={({ target }) => setEmail(target.value)} required placeholder="Escribe tu E-mail Aqui" />
+          <button onClick={createOrden}>Generar Orden</button>
         </form>
       </div>
     </div>
